65721bd1173faf06ff77188debb54c7c940465e1
Niveth_Saran <nivethsaran@gmail.com>
2020-11-20 22:49:47 +0530

    Modify app.py

--- app.py
+++ app.py
@@ -180,5 +180,7 @@ def execute():
         return json.loads(output)['output'].replace('\n', ' \r\n ')
 
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
+port = int(os.environ.get('PORT', 33507))
+app.run(host='0.0.0.0', port=port)
