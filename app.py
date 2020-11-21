538d557513bcd80812a8ed944c625dd986b7b35c
Niveth_Saran <nivethsaran@gmail.com>
2020-11-21 14:33:06 +0530

    Fixing build errors

--- app.py
+++ app.py
@@ -180,7 +180,5 @@ def execute():
         return json.loads(output)['output'].replace('\n', ' \r\n ')
 
 
-# if __name__ == '__main__':
-#     app.run()
-port = int(os.environ.get('PORT', 33507))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run()
